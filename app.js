a0f8936e930f14c8c84a7c36c4b2e62fb03a398c
Graham <graycanny@hotmail.com>
2021-11-26 13:17:27 +0000

    gray 2

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
+// global variables
+let question
+let correctAnswer
+
+// write a fetch request to bring back a question in a p
+async function getQuestion(){
+    let response = await fetch("https://opentdb.com/api.php?amount=10");
+    let data = await response.json();
+    console.log(data);
+    // retrieve question
+    question = document.querySelector('#question');
+    question.innerText = data.results[0]['question'];
+    //retrieve answer
+    correctAnswer = data.results[0]['correct_answer'];
+    console.log(correctAnswer)
+    return correctAnswer
+}
+
+// make btn that fetches---link btn, changing innerText to new Q
+let newQuestion = document.querySelector("#new-question");
+newQuestion.addEventListener('click', getQuestion);
+
+// Stores answer in 'answer' when submit clicked
+// check if answer is correct
+function checkAnswerInput () {
+    answer = document.getElementById("useranswer").value;
+    console.log(answer);
+    // if (answer === data.results[0]['correct_answer']){
+    //     console.log('Its working!');
+    // }
+}
+let submitButton = document.querySelector("#input-button")
+submitButton.addEventListener('click', checkAnswerInput)
+
 // ask their name to use in game?
 // check the answer and return r/w
 // give them a score at the end
 // ask if they'd like to play again
-
-// write a fetch request to bring back a question in a p
-
-getQuestion();
-
-async function getQuestion(){
-    let response = await fetch("https://opentdb.com/api.php?amount=10");
-    console.log(response);
-    let data = await response.json();
-    console.log(data)
-    let question = document.querySelector('#question')
-    question.innerText = data.results[0]['question']
-}
-
-getQuestion();
-
-// make a button that activates the fetch
-//link button from html to js, changing innerText to new question
-
-let newQuestion = document.querySelector("#new-question")
-newQuestion.addEventListener('click', getQuestion)
-
-// how does the user interact? clicking buttons/input?
 //create an input element to take user answer as string
 //check the answer againt the actually answer
 //return a true or false 
 //keep score 
-
-function getAnswerInput () {
-    let answer = document.getElementById("useranswer").value
-    return answer
-    
-}
-console.log(answer)
-
-
-let submitButton = document.querySelector("#input-button")
-submitButton.addEventListener('click', getAnswerInput)
