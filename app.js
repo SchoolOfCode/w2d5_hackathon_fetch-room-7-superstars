1ba65444962b7bdfc00767416a7d53b43e8e3d05
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2021-11-26 12:41:09 +0000

    current script

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
 // ask their name to use in game?
-// make a button that activates the fetch
-
-// how does the user interact? clicking buttons?
 // check the answer and return r/w
 // give them a score at the end
 // ask if they'd like to play again
 
 // write a fetch request to bring back a question in a p
+
+getQuestion();
+
 async function getQuestion(){
-    let response = await fetch('https://api.kanye.rest');
+    let response = await fetch("https://opentdb.com/api.php?amount=10");
     console.log(response);
     let data = await response.json();
-    console.log(data);
-    let question = document.querySelector('question')
-    question.innerText = data.results['quote']
+    console.log(data)
+    let question = document.querySelector('#question')
+    question.innerText = data.results[0]['question']
 }
 
 getQuestion();
 
-//write async function
-// async function getQuote(){
-//     let response = await fetch("https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=boolean");
-//     console.log(response);
-//     let data = await response.json();
-//     console.log(data);
-//     let kanyeQuote = document.querySelector('#t-question')
-//     kanyeQuote.innerText = data['quote'];
-//     addToList(data['quote'])
-// }
-// getQuote()
-//new quote button
-// let newQuoteButton = document.querySelector("#new-quote-button")
-//the event listener attached to button
-// newQuoteButton.addEventListener("click", getQuote)
+// make a button that activates the fetch
+//link button from html to js, changing innerText to new question
+
+let newQuestion = document.querySelector("#new-question")
+newQuestion.addEventListener('click', getQuestion)
+
+// how does the user interact? clicking buttons/input?
+//create an input element to take user answer as string
+//check the answer againt the actually answer
+//return a true or false 
+//keep score 
+
+function getAnswerInput () {
+    let answer = document.getElementById("useranswer").value
+    return answer
+    
+}
+console.log(answer)
+
+
+let submitButton = document.querySelector("#input-button")
+submitButton.addEventListener('click', getAnswerInput)
